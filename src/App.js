cf883c2da8dcd0dfb861c87fc9ad50de71735686
nadhe <nageshwar.adhe@amadeus.com>
2021-05-17 13:16:33 +0530

    Theme Basic implementation complete

--- src/App.js
+++ src/App.js
@@ -7,17 +7,8 @@ function App() {
     
     <React.Fragment>
       
-      <button type="button" className="btn btn-primary">Primary</button>
-                    <button type="button" className="btn btn-secondary">Secondary</button>
-                    <button type="button" className="btn btn-success">Success</button>
-                    <button type="button" className="btn btn-danger">Danger</button>
-                    <button type="button" className="btn btn-warning">Warning</button>
-                    <button type="button" className="btn btn-info">Info</button>
-                    <button type="button" className="btn btn-light">Light</button>
-                    <button type="button" className="btn btn-custom-color">custom-color</button>
-
-                    <button type="button" className="btn btn-link">Link</button>
-      {/* <ResumeLayout /> */}
+     
+      <ResumeLayout />
     </React.Fragment>
   );
 }
